d1e875b4dfcec2fcabeb500fa1c2b03d4dff2e44
tijou allan <tijouallan@gmail.com>
2023-06-08 09:01:01 +0200

    vue casier avec liste

--- vue_casier.py
+++ vue_casier.py
@@ -1,5 +1,10 @@
 from tkinter import *
 from tkinter import ttk
+#-------Fonction select sur la table----------
+def on_tree_select(event):
+    # Obtient l'identifiant de l'élément sélectionné
+    selected_item = my_tree.focus()
+
 
 fenetre = Tk()  # Génération de la fenêtre
 fenetre.resizable(width=TRUE, height=TRUE)
@@ -36,13 +41,42 @@ button1.pack(side=LEFT)
 button2 = Button(div, text="Quitter")
 button2.pack(side=LEFT)
 
+
 #-------liste de gauche----------
-my_tree = ttk.Treeview(fenetre)
+my_tree = ttk.Treeview(sous_div1)
 
 #definition des colonnes
-my_tree['colonnes'] = ("Id","élèves")
+my_tree['column'] = ("Id","élèves")
 
+my_tree.column("#0", width=0,stretch=NO)
 my_tree.column("Id",anchor=W, width=80)
 my_tree.column("élèves",anchor=W, width=80)
+
+my_tree.heading("Id", text="Id")
+my_tree.heading("élèves", text="Elèves")
+
+my_tree.insert(parent='',index='end',iid=0,values=(1,"tijou allan"))
+my_tree.insert(parent='',index='end',iid=1,values=(2,"viardot tibo"))
+my_tree.pack()
+
+#-------liste de droite----------
+
+#Checklist
+var1 = IntVar()
+var2 = IntVar()
+var3 = IntVar()
+
+check1 = Checkbutton(sous_div2, text="a oublié son badge", variable=var1)
+
+
+check2 = Checkbutton(sous_div2, text="ouvrir casier clé", variable=var2)
+
+
+check3 = Checkbutton(sous_div2, text="ouvrir casier téléphone", variable=var3)
+
+
+# Lie la fonction `on_tree_select` à l'événement de sélection d'un élément dans la table
+# Remplacez `my_tree` par votre propre référence à votre widget de liste
+my_tree.bind("<<TreeviewSelect>>", on_tree_select)
 
 fenetre.mainloop()
