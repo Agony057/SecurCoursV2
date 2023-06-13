020dff9f8a08eed587fc52cf299859ffa633d990
AGOZZINO Anthony <lyon.a@hotmail.fr>
2023-06-13 09:50:37 +0200

    maj

--- bdd/Script_bdd.sql
+++ bdd/Script_bdd.sql
@@ -18,7 +18,7 @@ DROP TABLE IF EXISTS Classe CASCADE;
 -- ---------------------------------------------------------------------------------------------------------------------
 -- 
 CREATE TABLE Classe(
-	id VARCHAR(10) NOT NULL CHECK(id IN('Vert', 'Bleu', 'Jaune', 'Rouge', 'Orange', 'Or', '')),
+	id VARCHAR(19) NOT NULL CHECK(id IN('Vert', 'Bleu', 'Jaune', 'Rouge', 'Orange', 'Or', 'Toutes_les_classes')),
     Libelle VARCHAR(25) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -64,23 +64,25 @@ INSERT INTO Classe VALUES ('Bleu', 'Seconde Bac Pro TMA'),
 						('Jaune', 'Première CAP MF'),
                         ('Orange', 'Terminale CAP MF'),
 						('Or', 'Professeur'),
-						('', 'Toutes les classes');
+						('Toutes_les_classes', 'Toutes les classes');
 --
-INSERT INTO Identite(NoCarte, Nom, Prenom, Classe_id, Statut, id) VALUES
-							('12-153-12-2', 'BEAUDOUIN', 'Corentin', 'Or', 'ADMIN', 'BEAUDOUIN1'),
-							('170-1-62-61', 'MENNINGER', 'Jason', 'Jaune', NULL, 'MENNINGER1'),
-							('64-105-61-61', 'ZELL', 'Renaud', 'Vert', NULL, 'ZELL1'),
-							('4-232-61-61', 'TIJOU', 'Allan', 'Orange', NULL, 'TIJOU1'),
-							('15-55-62-61', 'VIARDOT', 'Thibault', 'Rouge', NULL, 'VIARDOT1'),
-							('142-62-61-61', 'AGOZZINO', 'Anthony', 'Bleu', NULL, 'AGOZZINO1');
--- 
+INSERT INTO Identite(NoCarte, Nom, Prenom, Classe_id, Statut, id)
+VALUES ('12-153-12-2', 'BEAUDOUIN', 'Corentin', 'Or', 'ADMIN', 'BEAUDOUIN1');
+--
 INSERT INTO Casier(id, Classe_id) VALUES (1, 'Vert'), (2, 'Vert'),
                                          (3, 'Rouge'), (4, 'Jaune'),
                                          (5, 'Orange'), (6, 'Bleu');
 --
-INSERT INTO Log(id, Eleve_id, Recupere, Casier_id) VALUES (DATE_FORMAT(NOW(), '%d/%m/%y %H:%i:%s'), 'VIARDOT1', 'TEL', 1),
-                                                          (DATE_FORMAT(NOW(), '%d/%m/%y %H:%i:%s'), 'TIJOU1', 'CLE', 2),
-                                                          (DATE_FORMAT(NOW(), '%d/%m/%y %H:%i:%s'), 'MENNINGER1', 'RIEN', 1);
+INSERT INTO Identite(NoCarte, Nom, Prenom, Classe_id, Statut, id, Casier_id)
+VALUES ('170-1-62-61', 'MENNINGER', 'Jason', 'Jaune', NULL, 'MENNINGER1', 4),
+       ('64-105-61-61', 'ZELL', 'Renaud', 'Vert', NULL, 'ZELL1', 1),
+       ('4-232-61-61', 'TIJOU', 'Allan', 'Orange', NULL, 'TIJOU1', 5),
+       ('15-55-62-61', 'VIARDOT', 'Thibault', 'Rouge', NULL, 'VIARDOT1', 3),
+       ('142-62-61-61', 'AGOZZINO', 'Anthony', 'Bleu', NULL, 'AGOZZINO1', 6);
+--
+INSERT INTO Log(id, Eleve_id, Recupere, Casier_id) VALUES (DATE_FORMAT(NOW(), '%d/%m/%y %H:%i:%s'), 'VIARDOT1', 'TEL', 1);
+#                                                           (DATE_FORMAT(NOW(), '%d/%m/%y %H:%i:%s'), 'TIJOU1', 'CLE', 2),
+#                                                           (DATE_FORMAT(NOW(), '%d/%m/%y %H:%i:%s'), 'MENNINGER1', 'RIEN', 1);
 --
 -- -------------------------------------- || 2 select pour verifier les données des 2 tables || ------------------------
 -- 
