49c2fc6735efdcd44539a9fd49612c263f23ac2b
AGOZZINO Anthony <lyon.a@hotmail.fr>
2023-06-14 08:53:17 +0200

    erreur sur test bouton radio

--- vue_ajouter_eleve.py
+++ vue_ajouter_eleve.py
@@ -4,18 +4,18 @@ from tkinter import ttk
 fenetre = Tk()
 fenetre.resizable(width=TRUE, height=TRUE)
 
-div = Frame(fenetre)
-div.pack(padx=20,pady=10)  # Ajout du padding à gauche et à droite de 20 pixels
-div2 = Frame(fenetre)
-div2.pack(pady=25)
-div3 = Frame(fenetre)
-div3.pack(pady=10)
+div_vue_eleve = Frame(fenetre)
+div_vue_eleve.pack(padx=20, pady=10)  # Ajout du padding à gauche et à droite de 20 pixels
+div_vue_eleve2 = Frame(fenetre)
+div_vue_eleve2.pack(pady=25)
+div_vue_eleve3 = Frame(fenetre)
+div_vue_eleve3.pack(pady=10)
 
-sous_div1 = Frame(div2, height=200)
-sous_div1.grid(row=0, column=0, sticky="nsew")
+sous_div_vue_eleve1 = Frame(div_vue_eleve2, height=200)
+sous_div_vue_eleve1.grid(row=0, column=0, sticky="nsew")
 
-sous_div2 = Frame(div2)
-sous_div2.grid(row=0, column=1, sticky="nsew",pady=25)
+sous_div_vue_eleve2 = Frame(div_vue_eleve2)
+sous_div_vue_eleve2.grid(row=0, column=1, sticky="nsew", pady=25)
 
 # Fonction de mise à jour des données de la liste de gauche en fonction de la classe sélectionnée
 def update_data():
@@ -39,7 +39,7 @@ def update_data():
 
 
 # DropDownMenu
-variable = StringVar(div)
+variable = StringVar(div_vue_eleve)
 variable.set("Sélectionner une classe")  # Valeur par défaut
 
 options = ["Sélectionner une classe", "2AMA Seconde Bac Pro TMA", "1TMA Première Bac Pro TMA", "1MEN 1ère CAP MF", "TMEN Terminale CAP MF"]
@@ -48,78 +48,78 @@ options = ["Sélectionner une classe", "2AMA Seconde Bac Pro TMA", "1TMA Premiè
 def on_class_select(*args):
     update_data()
 
-select_classes = ttk.Combobox(div, textvariable=variable, values=options,width=30)
+select_classes = ttk.Combobox(div_vue_eleve, textvariable=variable, values=options, width=30)
 select_classes.pack(side=LEFT)
 select_classes.bind("<<ComboboxSelected>>", on_class_select)
 
 # BLANK
-blank_label = Label(div, width=50)
+blank_label = Label(div_vue_eleve, width=50)
 blank_label.pack(side=LEFT)
 
 # BtnEleve
-button1 = Button(div, text="Elèves")
+button1 = Button(div_vue_eleve, text="Elèves")
 button1.pack(side=LEFT)
 
 # BLANK
-blank_label = Label(div, width=10)
+blank_label = Label(div_vue_eleve, width=10)
 blank_label.pack(side=LEFT)
 
 # BtnQuitter
-button2 = Button(div, text="Quitter")
+button2 = Button(div_vue_eleve, text="Quitter")
 button2.pack(side=LEFT)
 #GRID
 
 # BtnAjouter
-btnAdd = Button(div3, text="Valider")
+btnAdd = Button(div_vue_eleve3, text="Valider")
 btnAdd.grid(row=0, column=0)
 
 # ScanBadge
-btnScan = Button(div3, text="Scan Badge")
+btnScan = Button(div_vue_eleve3, text="Scan Badge")
 btnScan.grid(row=0, column=1)
 
 #entry + lbl nom
-lblNom = Label(div3, text="Nom:")
+lblNom = Label(div_vue_eleve3, text="Nom:")
 lblNom.grid(row=0, column=3)
 
-entryNom = Entry(div3)
+entryNom = Entry(div_vue_eleve3)
 entryNom.grid(row=0, column=4)
 
 #entry + lbl prenom
-lblPrenom = Label(div3, text="Prenom:")
+lblPrenom = Label(div_vue_eleve3, text="Prenom:")
 lblPrenom.grid(row=0, column=5)
 
-entryPrenom = Entry(div3)
+entryPrenom = Entry(div_vue_eleve3)
 entryPrenom.grid(row=0, column=6)
 
 #entry + lbl NCasier
-lblNCasier = Label(div3, text="N° Casier:")
+lblNCasier = Label(div_vue_eleve3, text="N° Casier:")
 lblNCasier.grid(row=0, column=7)
 
-entryNCasier = Entry(div3)
+entryNCasier = Entry(div_vue_eleve3)
 entryNCasier.grid(row=0, column=8)
 
 # DropDownMenu
-variable_new_eleve_class = StringVar(div3)
+variable_new_eleve_class = StringVar(div_vue_eleve3)
 variable_new_eleve_class.set("Sélectionner une classe")  # Valeur par défaut
 
 options_new_eleve_class = ["Sélectionner une classe", "2AMA Seconde Bac Pro TMA", "1TMA Première Bac Pro TMA", "1MEN 1ère CAP MF", "TMEN Terminale CAP MF"]
-new_eleve_class = ttk.Combobox(div3, textvariable=variable_new_eleve_class, values=options_new_eleve_class, width=20)
+new_eleve_class = ttk.Combobox(div_vue_eleve3, textvariable=variable_new_eleve_class, values=options_new_eleve_class, width=20)
 new_eleve_class.grid(row=0, column=9)
 
 # BtnAnnuler
-btnAnnuler = Button(div3, text="Annuler")
+btnAnnuler = Button(div_vue_eleve3, text="Annuler")
 btnAnnuler.grid(row=1, column=0)
 
 #entry + lbl IUD
-lblIUD = Label(div3, text="IUD:")
+lblIUD = Label(div_vue_eleve3, text="IUD:")
 lblIUD.grid(row=1, column=1)
 
-entryIUD = Entry(div3)
+entryIUD = Entry(div_vue_eleve3)
 entryIUD.grid(row=1, column=2)
 
 
 # Liste de gauche
-my_tree = ttk.Treeview(sous_div1)
+my_tree = ttk.Treeview(sous_div_vue_eleve1)
 my_tree['columns'] = ("Id", "nom", "prenom")
 my_tree.column("#0", width=0, stretch=NO)
 my_tree.column("Id", anchor=W, width=80)
