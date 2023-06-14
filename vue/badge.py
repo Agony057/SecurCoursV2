b323ea4a43d7af2ccc286f1db55d5fd3cc57a84a
AGOZZINO Anthony <lyon.a@hotmail.fr>
2023-06-14 10:26:46 +0200

    modifiaction vue eleve

--- vue/badge.py
+++ vue/badge.py
@@ -5,10 +5,10 @@ from tkinter import *
 def accueil():
     fenetre = Tk()
 
-    label = Label(fenetre, text="Veuillez passez votre badge")
+    label_accueil = Label(fenetre, text="Veuillez passez votre badge")
     # label.pack(pady=20, padx=20)
-    label.pack()
+    label_accueil.pack()
     fenetre.geometry("400x300")
-    label.place(relx=0.5, rely=0.5, anchor=CENTER)
+    label_accueil.place(relx=0.5, rely=0.5, anchor=CENTER)
 
     fenetre.mainloop()
